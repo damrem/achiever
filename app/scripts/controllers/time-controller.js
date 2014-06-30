4d5eef424de0ae1c4fb0c58cff4f78b09f7646a9
damrem <damien.remars@gmail.com>
2014-06-30 14:38:56 +0200

    TimeControllers inherits from SuperController

--- app/scripts/controllers/time-controller.js
+++ app/scripts/controllers/time-controller.js
@@ -14,60 +14,16 @@
 angular.module('app')
 
     .controller('TimeController',
-            ['$scope', '$interval', '$cookies', 'fibonacci', 'levelConverter',
-            function ($scope, $interval, $cookies, fibonacci, levelConverter) {
+            ['$scope', '$interval', '$cookies', 'levelConverter', '$controller',
+            function ($scope, $interval, $cookies, levelConverter, $controller) {
 
-                $scope.debug = '';
-
-                var factor = 10;
-
-                function getCurrentLevel() {
-                    return levelConverter.getLevelFromValue($scope.total, factor);
-                }
-
-                function getNextLevel() {
-                    return getCurrentLevel() + 1;
-                }
-
-                function updateLevel() {
-                    $scope.level = getCurrentLevel();
-                }
-
-                function updateTotal() {
-                    $scope.totalInLevel = 
-                        levelConverter.getValueFromLevel(getNextLevel(), factor) - levelConverter.getValueFromLevel(getCurrentLevel(), factor);
-                }
-
-                function setup() {
-                    //  no cookie yet, we set one up
-                    if ($cookies.timeElapsed === undefined) {
-                        $cookies.timeElapsed = 0;
-                    }
-
-                    //  we retrieve the total time from the cookie
-                    $scope.total = parseInt($cookies.timeElapsed, 10);
-                    updateLevel();
-                    updateTotal();
-                    $scope.currentInLevel = $scope.total - levelConverter.getValueFromLevel(getCurrentLevel(), factor);
-                }
-
-                function updateTime() {
-                    $scope.total += 1;
-                    $cookies.timeElapsed = $scope.total;
-                    $scope.currentInLevel += 1;
-
-                    if ($scope.currentInLevel > $scope.totalInLevel) {
-                        $scope.currentInLevel = 1;
-                        updateLevel();
-                        updateTotal();
-                    }
-                }
-
-                setup();
+                $controller('SuperController', 
+                    {$scope: $scope, $cookies: $cookies, levelConverter: levelConverter, 
+                        factor: 10, propName: 'timeElapsed'});
 
                 $interval(function () {
 
-                    updateTime();
+                    $scope.increment();
 
                 }, 1000);
 
