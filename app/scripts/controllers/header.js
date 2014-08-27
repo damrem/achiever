1233a0d99f49f2db300e5236dcbe7eeeb56e99fa
damrem <damien.remars@gmail.com>
2014-08-27 18:23:26 +0200

    header: the active page link now depends on the location of the page. resolved the refresh bug on about page

--- app/scripts/controllers/header.js
+++ app/scripts/controllers/header.js
@@ -3,8 +3,29 @@
 	
 	angular.module('app')
 	
-		.controller('HeaderController', ['$scope', function ($scope) {
+		.controller('HeaderController', ['$scope', '$rootScope', '$route', '$location', '$log', 
+		function ($scope, $rootScope, $route, $location, $log) {
 		
+		/*
+			$rootScope.$on('$routeChangeStart', function(event, current, previous, rejection){
+				$log.log('$routeChangeStart', $scope, $rootScope, $route, $location);
+				//switch()
+			});
+*/
+			$rootScope.$on('$routeChangeSuccess', function(event, current){
+				//$log.log('$routeChangeSuccess', $scope, $rootScope, $route, $location);
+				$log.log('$routeChangeSuccess', current.originalPath);
+				var elt;
+				switch(current.originalPath){
+					case '/':elt='home';break;
+					case '/about':elt='about';break;
+				}
+				angular.element('#home').removeClass('active');
+				angular.element('#about').removeClass('active');
+				angular.element('#' + elt).addClass('active');
+			});
+
+/*
 			$scope.highlight = function (elt) {
 				angular.element('#home').removeClass('active');
 				angular.element('#about').removeClass('active');
@@ -13,6 +34,7 @@
 		
                 
 			};
+			*/
 		}]);
 	
 }());
